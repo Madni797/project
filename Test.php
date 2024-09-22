97be39ccf14b7fd7f906a502c10cab40d2714a88
HamzaShabbir07 <hamxashabir007@gmail.com>
2024-09-22 15:52:13 +0500

    test

---
+++ Test.php
@@ -0,0 +1,29 @@
+<?php
+// echo "Test Case Updated cases";
+
+// $a = 2;
+// $b = 3;
+// $c = $a + $b; 
+// echo "Sum of Two Values:", $c; 
+
+// $age = 22;
+// if($age> 20){
+//     echo "Your age is Greater than 20";
+// }
+// else {
+//  echo "Your age is not Greater than 20";
+// }
+
+
+// for ($i=0; $i<10; $i++ ){
+// echo "For Loop" . $i . "<br>";
+// }
+
+function test($name, $age){
+    echo "Hi" . " " . $name  . " " . $age . "<br>"; 
+}
+test("Ali", 16);
+test("Raza", 17);
+
+
+?>
